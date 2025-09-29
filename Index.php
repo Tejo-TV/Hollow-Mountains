6dc32e67d7ff67d448e296de31f78ed064f6de18
Tejo-TV <150041189+Tejo-TV@users.noreply.github.com>
2025-09-29 12:03:40 +0200

    UPDATE: homepage done
    duurde joris 3 weken en ik doe het in 5 min gotv het is nu klaar!!!!!!

--- Index.php
+++ Index.php
@@ -2,8 +2,25 @@
 //---------------------------------------------------------------------------------------------------//
 // Naam script		  : index.php
 // Omschrijving		  : Dit is de homepagina
-// Naam ontwikkelaar  : Joris Hoos
+// Naam ontwikkelaar  : Joris Hoos, Tejo Veldman
 // Project		      : Hollow Mountains
 // Datum		      : Schooljaar 3 - periode 1 - 2025
 //---------------------------------------------------------------------------------------------------//
 ?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hollow Mountains Dashboard</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="homepage-container">
+        <h1>Hollow Mountains Dashboard</h1>
+        <p>Het centrale dashboard voor uw petpark</p>
+        <img src="assets/images/Hollow-Mountains.png" alt="Petpark" class="homepage-image">
+        <a href="pages/login.php" class="homepage-button">Inloggen</a>
+    </div>
+</body>
+</html>
